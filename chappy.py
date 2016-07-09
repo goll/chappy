e98a66bb6ebd18a815b04b207b8b45b65a501377
Mladen Zekanovic <mladen.zekanovic@gmail.com>
2016-07-10 00:18:19 +0200

    Refactoring for difference between json and txt route

--- chappy.py
+++ chappy.py
@@ -5,108 +5,156 @@ import hashlib
 import aiohttp
 from aiohttp import web
 from passlib import hash
+from pprint import pprint
+
+def text_or_json(is_json=False, data_dict=None):
+
+    if data_dict is None:
+        data_dict = {}
+
+    if is_json:
+
+        return web.json_response(data_dict)
+    else:
+        body = "<h1>{}</h1>".format(data_dict['hash'] if data_dict['success'] else data_dict['reason'])
+
+        return web.Response(body=body.encode('utf-8'))
+        
 
 def hashlib_view_factory(algorithm):
 
+
     async def handler(request):
+
+        is_json = False
+        if 'json' in request.path[-4:]:
+            is_json = True;
 
         if request.method == 'POST':
             await request.post()
+
             data = request.POST.get('data')
-            if len(data) > 64:
-                return web.json_response({'success': False, 'reason': 'data is longer than 64 characters'})
+            if data is not None and len(data) > 64:
+
+                return text_or_json(is_json, {'success': False, 'reason': 'data is longer than 64 characters'})
         elif request.method == 'GET':
             data = request.GET.get('data')
-            if len(data) > 64:
-                return web.json_response({'success': False, 'reason': 'data is longer than 64 characters'})
+            if data is not None and len(data) > 64:
+
+                return text_or_json(is_json, {'success': False, 'reason': 'data is longer than 64 characters'})
         else:
-            return web.json_response({'success': False, 'reason': 'method not allowed'})
+
+            return text_or_json(is_json, {'success': False, 'reason': 'method not allowed'})
 
         if data is None:
-            return web.json_response({'success': False, 'reason': 'missing data parameter'})
+
+            return text_or_json(is_json, {'success': False, 'reason': 'missing data parameter'})
 
         hash_class = getattr(hashlib, algorithm)
         hc = hash_class()
         hc.update(data.encode('utf-8'))
 
-        return web.json_response({'success': True, 'hash': hc.hexdigest(), 'algorithm': algorithm })
+        return text_or_json(is_json, {'success': True, 'hash': hc.hexdigest(), 'algorithm': algorithm })
 
     return handler
+
 
 def passlib_view_factory(algorithm, **kwds):
 
     async def handler(request):
 
+        is_json = False
+        if 'json' in request.path[-4:]:
+            is_json = True;
+
         if request.method == 'POST':
             await request.post()
+
             data = request.POST.get('data')
-            if len(data) > 64:
-                return web.json_response({'success': False, 'reason': 'data is longer than 64 characters'})
+            if data is not None and len(data) > 64:
+
+                return text_or_json(is_json, {'success': False, 'reason': 'data is longer than 64 characters'})
         elif request.method == 'GET':
             data = request.GET.get('data')
-            if len(data) > 64:
-                return web.json_response({'success': False, 'reason': 'data is longer than 64 characters'})
+            if data is not None and len(data) > 64:
+
+                return text_or_json(is_json, {'success': False, 'reason': 'data is longer than 64 characters'})
         else:
-            return web.json_response({'success': False, 'reason': 'method not allowed'})
+
+            return text_or_json(is_json, {'success': False, 'reason': 'method not allowed'})
 
         if data is None:
-            return web.json_response({'success': False, 'reason': 'missing data parameter'})
+
+            return text_or_json(is_json, {'success': False, 'reason': 'missing data parameter'})
 
         hash_function = getattr(hash, algorithm)
 
-        return web.json_response({'success': True, 'hash': hash_function.encrypt(data.encode('utf-8'), **kwds), 'algorithm': algorithm })
-
+        return text_or_json(is_json, {'success': True, 'hash': hash_function.encrypt(data.encode('utf-8'), **kwds), 'algorithm': algorithm })
+        
     return handler
+
 
 app = web.Application()
 
+
+def add_html_and_json_route(methods, path, views):
+        
+    if path[-1] != '/':
+        json_path = "{}/json".format(path)
+    else:
+        json_path = "{}json".format(path)
+
+    app.router.add_route(methods, path, views)
+    app.router.add_route(methods, json_path, views)
+
+
 # hashlib
-app.router.add_route('*', '/md5.json', hashlib_view_factory('md5'))
-app.router.add_route('*', '/sha1.json', hashlib_view_factory('sha1'))
-app.router.add_route('*', '/sha224.json', hashlib_view_factory('sha224'))
-app.router.add_route('*', '/sha256.json', hashlib_view_factory('sha256'))
-app.router.add_route('*', '/sha384.json', hashlib_view_factory('sha384'))
-app.router.add_route('*', '/sha512.json', hashlib_view_factory('sha512'))
+add_html_and_json_route('*', '/md5', hashlib_view_factory('md5'))
+add_html_and_json_route('*', '/sha1', hashlib_view_factory('sha1'))
+add_html_and_json_route('*', '/sha224', hashlib_view_factory('sha224'))
+add_html_and_json_route('*', '/sha256', hashlib_view_factory('sha256'))
+add_html_and_json_route('*', '/sha384', hashlib_view_factory('sha384'))
+add_html_and_json_route('*', '/sha512', hashlib_view_factory('sha512'))
 
 # crypt
-app.router.add_route('*', '/shadow.json', passlib_view_factory('sha512_crypt', rounds=65536))
-app.router.add_route('*', '/htpasswd.json', passlib_view_factory('apr_md5_crypt'))
-app.router.add_route('*', '/md5-crypt.json', passlib_view_factory('md5_crypt'))
-app.router.add_route('*', '/bcrypt.json', passlib_view_factory('bcrypt'))
-app.router.add_route('*', '/sha1-crypt.json', passlib_view_factory('sha1_crypt', rounds=65536))
-app.router.add_route('*', '/sha256-crypt.json', passlib_view_factory('sha256_crypt', rounds=65536))
-app.router.add_route('*', '/sha512-crypt.json', passlib_view_factory('sha512_crypt', rounds=65536))
-app.router.add_route('*', '/apr-md5-crypt.json', passlib_view_factory('apr_md5_crypt'))
-app.router.add_route('*', '/bcrypt-sha256.json', passlib_view_factory('bcrypt_sha256'))
-app.router.add_route('*', '/pbkdf2-sha1.json', passlib_view_factory('pbkdf2_sha1', rounds=65536))
-app.router.add_route('*', '/pbkdf2-sha256.json', passlib_view_factory('pbkdf2_sha256'))
-app.router.add_route('*', '/pbkdf2-sha512.json', passlib_view_factory('pbkdf2_sha512'))
+add_html_and_json_route('*', '/shadow', passlib_view_factory('sha512_crypt', rounds=65536))
+add_html_and_json_route('*', '/htpasswd', passlib_view_factory('apr_md5_crypt'))
+add_html_and_json_route('*', '/md5-crypt', passlib_view_factory('md5_crypt'))
+add_html_and_json_route('*', '/bcrypt', passlib_view_factory('bcrypt'))
+add_html_and_json_route('*', '/sha1-crypt', passlib_view_factory('sha1_crypt', rounds=65536))
+add_html_and_json_route('*', '/sha256-crypt', passlib_view_factory('sha256_crypt', rounds=65536))
+add_html_and_json_route('*', '/sha512-crypt', passlib_view_factory('sha512_crypt', rounds=65536))
+add_html_and_json_route('*', '/apr-md5-crypt', passlib_view_factory('apr_md5_crypt'))
+add_html_and_json_route('*', '/bcrypt-sha256', passlib_view_factory('bcrypt_sha256'))
+add_html_and_json_route('*', '/pbkdf2-sha1', passlib_view_factory('pbkdf2_sha1', rounds=65536))
+add_html_and_json_route('*', '/pbkdf2-sha256', passlib_view_factory('pbkdf2_sha256'))
+add_html_and_json_route('*', '/pbkdf2-sha512', passlib_view_factory('pbkdf2_sha512'))
 
 # LDAP
-app.router.add_route('*', '/ldap.json', passlib_view_factory('ldap_sha512_crypt', rounds=65536))
-app.router.add_route('*', '/ldap-md5.json', passlib_view_factory('ldap_md5'))
-app.router.add_route('*', '/ldap-sha1.json', passlib_view_factory('ldap_sha1'))
-app.router.add_route('*', '/ldap-salted-md5.json', passlib_view_factory('ldap_salted_md5'))
-app.router.add_route('*', '/ldap-salted-sha1.json', passlib_view_factory('ldap_salted_sha1'))
-app.router.add_route('*', '/ldap-md5-crypt.json', passlib_view_factory('ldap_md5_crypt'))
-app.router.add_route('*', '/ldap-bcrypt.json', passlib_view_factory('ldap_bcrypt'))
-app.router.add_route('*', '/ldap-sha1-crypt.json', passlib_view_factory('ldap_sha1_crypt', rounds=65536))
-app.router.add_route('*', '/ldap-sha256-crypt.json', passlib_view_factory('ldap_sha256_crypt', rounds=65536))
-app.router.add_route('*', '/ldap-sha512-crypt.json', passlib_view_factory('ldap_sha512_crypt', rounds=65536))
-app.router.add_route('*', '/ldap-hex-md5.json', passlib_view_factory('ldap_hex_md5'))
-app.router.add_route('*', '/ldap-hex-sha1.json', passlib_view_factory('ldap_hex_sha1'))
-app.router.add_route('*', '/ldap-pbkdf2-sha1.json', passlib_view_factory('ldap_pbkdf2_sha1', rounds=65536))
-app.router.add_route('*', '/ldap-pbkdf2-sha256.json', passlib_view_factory('ldap_pbkdf2_sha256'))
-app.router.add_route('*', '/ldap-pbkdf2-sha512.json', passlib_view_factory('ldap_pbkdf2_sha512'))
+add_html_and_json_route('*', '/ldap', passlib_view_factory('ldap_sha512_crypt', rounds=65536))
+add_html_and_json_route('*', '/ldap-md5', passlib_view_factory('ldap_md5'))
+add_html_and_json_route('*', '/ldap-sha1', passlib_view_factory('ldap_sha1'))
+add_html_and_json_route('*', '/ldap-salted-md5', passlib_view_factory('ldap_salted_md5'))
+add_html_and_json_route('*', '/ldap-salted-sha1', passlib_view_factory('ldap_salted_sha1'))
+add_html_and_json_route('*', '/ldap-md5-crypt', passlib_view_factory('ldap_md5_crypt'))
+add_html_and_json_route('*', '/ldap-bcrypt', passlib_view_factory('ldap_bcrypt'))
+add_html_and_json_route('*', '/ldap-sha1-crypt', passlib_view_factory('ldap_sha1_crypt', rounds=65536))
+add_html_and_json_route('*', '/ldap-sha256-crypt', passlib_view_factory('ldap_sha256_crypt', rounds=65536))
+add_html_and_json_route('*', '/ldap-sha512-crypt', passlib_view_factory('ldap_sha512_crypt', rounds=65536))
+add_html_and_json_route('*', '/ldap-hex-md5', passlib_view_factory('ldap_hex_md5'))
+add_html_and_json_route('*', '/ldap-hex-sha1', passlib_view_factory('ldap_hex_sha1'))
+add_html_and_json_route('*', '/ldap-pbkdf2-sha1', passlib_view_factory('ldap_pbkdf2_sha1', rounds=65536))
+add_html_and_json_route('*', '/ldap-pbkdf2-sha256', passlib_view_factory('ldap_pbkdf2_sha256'))
+add_html_and_json_route('*', '/ldap-pbkdf2-sha512', passlib_view_factory('ldap_pbkdf2_sha512'))
 
 # GRUB
-app.router.add_route('*', '/grub.json', passlib_view_factory('md5_crypt'))
-app.router.add_route('*', '/grub2.json', passlib_view_factory('grub_pbkdf2_sha512'))
+add_html_and_json_route('*', '/grub', passlib_view_factory('md5_crypt'))
+add_html_and_json_route('*', '/grub2', passlib_view_factory('grub_pbkdf2_sha512'))
 
 # Django
-app.router.add_route('*', '/django-bcrypt.json', passlib_view_factory('django_bcrypt'))
-app.router.add_route('*', '/django-pbkdf2-sha1.json', passlib_view_factory('django_pbkdf2_sha1', rounds=65536))
-app.router.add_route('*', '/django-pbkdf2-sha256.json', passlib_view_factory('django_pbkdf2_sha256'))
+add_html_and_json_route('*', '/django-bcrypt', passlib_view_factory('django_bcrypt'))
+add_html_and_json_route('*', '/django-pbkdf2-sha1', passlib_view_factory('django_pbkdf2_sha1', rounds=65536))
+add_html_and_json_route('*', '/django-pbkdf2-sha256', passlib_view_factory('django_pbkdf2_sha256'))
 
 if __name__ == '__main__':
 
